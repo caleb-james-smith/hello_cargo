c7f00df1414cf7737c6eafa85b9cc4915b7e0b48
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-07-20 17:55:47 -0500

    Add main and tools rust files. Demonstrate examples of importing functions from one file to another.

---
+++ src/tools.rs
@@ -0,0 +1,17 @@
+// tools.rs
+
+// method_1
+pub fn method_1() {
+    println!(" - Running method_1() from tools.rs.");
+}
+
+// method_2
+pub fn method_2() {
+    println!(" - Running method_2() from tools.rs.");
+}
+
+// method_3
+pub fn method_3() {
+    println!(" - Running method_3() from tools.rs.");
+}
+
